5ea3b04c0cfd36fc1af51744bc807ed28e3b64c8
a13576301231 <2963663242@qq.com>
2023-08-27 03:22:39 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fmt/core.h>
+#include <json/json.h>
+
+int main() {
+    int num = 42;
+    std::string text = "Hello, fmt!";
+    
+    // 使用 fmt 库进行格式化输出
+    std::string formatted = fmt::format("Number: {}, Text: {}", num, text);
+    
+    std::cout << formatted << std::endl;
+    
+        // 创建 JSON 对象
+    Json::Value root;
+    root["name"] = "John";
+    root["age"] = 30;
+    root["city"] = "New York";
+
+    // 将 JSON 对象转换为 JSON 字符串
+    Json::StreamWriterBuilder writer;
+    std::string json_str = Json::writeString(writer, root);
+
+    std::cout << "JSON String: " << json_str << std::endl;
+    return 0;
+}
